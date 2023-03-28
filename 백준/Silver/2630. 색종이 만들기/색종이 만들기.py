d0eb10ad28ff42d29ed24c056a3478e6d2d02920
Chun Hyunwoo <1000chw@gmail.com>
2023-03-28 18:41:19 +0900

    [Silver II] Title: 색종이 만들기, Time: 64 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -1,31 +1,32 @@
 import sys
-
-N = int(sys.stdin.readline())
+input = sys.stdin.readline
+N = int(input())
 arr = []
 for i in range(N):
-    li = list(map(int, sys.stdin.readline().split()))
+    li = list(map(int, input().split()))
     arr.append(li)
 
 res = [0, 0]
 
 
-def solve(_rt, _rb, _ct, _cb, res):
-    if all(arr[x][y] == 1 for y in range(_ct, _cb+1) for x in range(_rt, _rb+1)):
-        res[1] += 1
-        return
-    elif all(arr[x][y] == 0 for y in range(_ct, _cb+1) for x in range(_rt, _rb+1)):
-        res[0] += 1
-        return
+def solve(_rt, _rb, _ct, _cb):
+    tmp = set()
+    for x in range(_rt, _rb+1):
+        for y in range(_ct, _cb+1):
+            tmp.add(arr[x][y])
+    if len(tmp) == 1:
+        if list(tmp)[0]:
+            res[1] += 1
+        else: res[0] += 1
     else:
-        solve(_rt, (_rt+_rb)//2, _ct, (_ct+_cb)//2, res)
-        solve(_rt, (_rt+_rb)//2, (_ct+_cb)//2 + 1, _cb, res)
-        solve((_rt+_rb)//2 + 1, _rb, _ct, (_ct+_cb)//2, res)
-        solve((_rt+_rb)//2 + 1, _rb, (_ct+_cb)//2 + 1, _cb, res)
+        solve(_rt, (_rt+_rb)//2, _ct, (_ct+_cb)//2)
+        solve(_rt, (_rt+_rb)//2, (_ct+_cb)//2 + 1, _cb)
+        solve((_rt+_rb)//2 + 1, _rb, _ct, (_ct+_cb)//2)
+        solve((_rt+_rb)//2 + 1, _rb, (_ct+_cb)//2 + 1, _cb)
         return
 
 
-solve(0, N-1, 0, N-1, res)
+solve(0, N-1, 0, N-1)
 
 print(res[0])
 print(res[1])
-
