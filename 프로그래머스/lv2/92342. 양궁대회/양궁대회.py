bdc941d919d77870c61eb5794482e63e3a1b0d54
Chun Hyunwoo <1000chw@gmail.com>
2023-03-29 14:41:23 +0900

    [level 2] Title: 양궁대회, Time: 0.95 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/92342. 양궁대회/양궁대회.py
+++ 프로그래머스/lv2/92342. 양궁대회/양궁대회.py
@@ -1,28 +1,23 @@
 def solution(n, info):
+    p_score = 0
+    for i in range(10):
+        if info[i]:
+            p_score += 10-i
     answer = []
     arrows_dict = {}
     res = [-1]
-    Gap(info, 0, n, [], res, arrows_dict)
+    Gap(info, 0, n, [], res, arrows_dict, 0, p_score)
     if res == [-1]:
         answer.append(-1)
     else:
-        max_gap = sorted(arrows_dict.keys(), reverse=True)[0]
-        answer = list(reversed(list(map(int, list(str(max([int("".join(map(str, reversed(i)))) for i in arrows_dict[max_gap]])))))))
+        answer = list(reversed(list(map(int, list(str(max([int("".join(map(str, reversed(i)))) for i in arrows_dict[res[0]]])))))))
         answer += [0]*(11-len(answer))
     return answer
 
-def Gap(info, i, n, arrows, res, arrows_dict):
+def Gap(info, i, n, arrows, res, arrows_dict, r_score, p_score):
     if n == 0:
-        p_score = 0
-        r_score = 0
         x = arrows + [0] * (11 - i)
-        for j in range(10):
-            if info[j] < x[j]:
-                r_score += 10 - j
-            elif info[j] > x[j]:
-                p_score += 10 - j
         score_gap = r_score - p_score
-
         if score_gap > 0:
             if score_gap > res[0]:
                 arrows_dict[score_gap] = [x]
@@ -32,12 +27,12 @@ def Gap(info, i, n, arrows, res, arrows_dict):
         return
     elif i == 10:
         arrows.append(n)
-        Gap(info, i + 1, 0, [] + arrows, res, arrows_dict)
+        Gap(info, i + 1, 0, [] + arrows, res, arrows_dict, r_score, p_score)
     else:
         arrow = info[i] + 1
         if arrow > n:
-            Gap(info, i + 1, n, [] + arrows+[0], res, arrows_dict)
+            Gap(info, i + 1, n, [] + arrows+[0], res, arrows_dict, r_score, p_score)
             return
-        Gap(info, i + 1, n, [] + arrows+[0], res, arrows_dict)
+        Gap(info, i + 1, n, [] + arrows+[0], res, arrows_dict, r_score, p_score)
         arrows.append(arrow)
-        Gap(info, i + 1, n - arrow, [] + arrows, res, arrows_dict)
+        Gap(info, i + 1, n - arrow, [] + arrows, res, arrows_dict,r_score+10-i, p_score-10+i if info[i] else p_score)
