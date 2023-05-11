4dc0f41d652df783e5613a9788cd87f3e9865ef6
Chun Hyunwoo <1000chw@gmail.com>
2023-05-11 18:48:52 +0900

    [Silver I] Title: 단지번호붙이기, Time: 40 ms, Memory: 31348 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,28 +1,26 @@
 N = int(input())
 arr = []
 for i in range(N):
-    x = list(input())
-    arr.append(list(map(int, x)))
-chk_li = [[0]*N]*N
+    arr.append(list(map(int, list(input()))))
+
 x_ = [0, 1, 0, -1]
 y_ = [1, 0, -1, 0]
 
-def bfs(row, col):
+def dfs(row, col):
     res = 0
     arr[row][col] = 0
     for i in range(4):
         next_r = row + x_[i]
         next_c = col + y_[i]
         if 0 <= next_c < N and 0 <= next_r < N and arr[next_r][next_c]:
-            res += bfs(next_r, next_c)
+            res += dfs(next_r, next_c)
     return res + 1
 
 result = []
-while arr != chk_li:
-    for i in range(len(arr)):
-        if 1 in arr[i]:
-            result.append(bfs(i, arr[i].index(1)))
-            break
+for i in range(N):
+	for j in range(N):
+		if arr[i][j]: 
+			result.append(dfs(i, j))
 result.sort()
 print(len(result))
 for i in result:
