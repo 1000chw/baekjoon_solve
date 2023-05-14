b7d480088582f419495197094b9e29b830a37061
Chun Hyunwoo <1000chw@gmail.com>
2023-05-14 16:06:58 +0900

    [level 3] Title: 캠핑, Time: 3031.15 ms, Memory: 4.48 MB -BaekjoonHub

--- 프로그래머스/lv3/1833. 캠핑/캠핑.cpp
+++ 프로그래머스/lv3/1833. 캠핑/캠핑.cpp
@@ -10,7 +10,6 @@ bool comp(vector<int> x, vector<int> y) {
 // 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
 int solution(int n, vector<vector<int>> data) {
     int answer = 0;
-    sort(data.begin(), data.end());
     sort(data.begin(), data.end(), comp);
 
     bool chk = true;
