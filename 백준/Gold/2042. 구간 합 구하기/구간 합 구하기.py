1cb322ba345a5680588e1e2b44eb205d2437a416
Chun Hyunwoo <1000chw@gmail.com>
2023-06-22 21:02:00 +0900

    [Gold I] Title: 구간 합 구하기, Time: 1416 ms, Memory: 188288 KB -BaekjoonHub

---
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
@@ -0,0 +1,48 @@
+import sys
+input = sys.stdin.readline
+
+leaf = []
+tree = [0] * 10000000
+
+
+def makeTree(node, start, end):
+    if start == end:
+        tree[node] = leaf[start]
+    else:
+        tree[node] = makeTree(node*2, start, (start+end)//2) + makeTree(node*2+1, (start + end)//2+1, end)
+    return tree[node]
+
+
+def sumTree(node, start, end, left, right):
+    if start > right or end < left:
+        return 0
+
+    if left <= start and end <= right:
+        return tree[node]
+
+    return sumTree(node*2, start, (start+end)//2, left, right) + sumTree(node*2+1, (start+end)//2+1, end, left, right)
+
+
+def updateTree(node, start, end, ind, num):
+    if start <= ind <= end:
+        tree[node] += num
+        if start != end:
+            updateTree(node*2, start, (start+end)//2, ind, num)
+            updateTree(node * 2+1, (start + end) // 2 + 1, end, ind, num)
+
+
+N, M, K = map(int, input().split())
+
+for _ in range(N):
+    leaf.append(int(input()))
+
+makeTree(1, 0, N-1)
+
+for _ in range(M+K):
+    a, b, c = map(int, input().split())
+    if a == 1:
+        num = c - leaf[b-1]
+        leaf[b-1] = c
+        updateTree(1, 0, N-1, b-1, num)
+    else:
+        print(sumTree(1, 0, N-1, b-1, c-1))
