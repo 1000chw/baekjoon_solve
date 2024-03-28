e1e8dc90acb8a0c6c62f2cfd070bc66f2bbf7341
Chun Hyunwoo <1000chw@gmail.com>
2024-03-28 14:35:39 +0900

    [Platinum V] Title: 주유소, Time: 68 ms, Memory: 34528 KB, Score: 18 point -BaekjoonHub

---
+++ 백준/Platinum/13308. 주유소/주유소.py
@@ -0,0 +1,48 @@
+from collections import defaultdict
+from heapq import heappop, heappush
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+lp = [0] + list(map(int, input().split()))
+cities = defaultdict(list)
+answer = float('inf')
+
+def goToN(cur, n):
+    visited = [float('inf')]*(n+1)
+    visited[cur] = 0
+    q = [(0, cur)]
+    while q:
+        cost, v = heappop(q)
+        if visited[v] != cost:
+            continue
+        if v == n:
+            continue
+        for nv, c in cities[v]:
+            if visited[nv] > c + cost:
+                visited[nv] = c + cost
+                heappush(q, (c + cost, nv))
+
+    return visited
+
+
+for i in range(m):
+    x, y, c = map(int, input().split())
+    cities[x].append((y, c))
+    cities[y].append((x, c))
+
+times = [[]]
+for i in range(1, n):
+    timeToN = list(map(lambda x: x * lp[i], goToN(i, n)))
+    times.append(timeToN)
+
+
+def solve(cur, n, cost):
+    global answer
+    for i in range(1, n):
+        if lp[i] < lp[cur]:
+            solve(i, n, cost + times[cur][i])
+    answer = min(answer, cost + times[cur][n])
+
+solve(1, n, 0)
+print(answer)
