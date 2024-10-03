36127a7be760ee1f33ed3c5b7c9d3eed211a2787
Chun Hyunwoo <1000chw@gmail.com>
2024-10-03 20:21:57 +0900

    [level 2] Title: 전화번호 목록, Time: 152.79 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,27 +1,6 @@
 function solution(phone_book) {
-  let answer = true;
-  const map = new Map();
-  phone_book.forEach(phone => {
-    let cur = map;
-    for (let i = 0; i < phone.length; i++) {
-      const p = phone.at(i);
-      if (cur.get(p) === 1) {
-        answer = false;
-        break;
-      }
-      if (i === phone.length - 1) {
-        if (cur.get(p)) {
-          answer = false;
-        }
-        cur.set(p, 1);
-      }
-      else if (cur.get(p)) cur = cur.get(p);
-      else {
-        cur.set(p, new Map());
-        cur = cur.get(p);
-      }
-    }
-
+  return !phone_book.sort().some((t, i) => {
+    if (i === phone_book.length - 1) return false;
+    return phone_book[i+1].startsWith(t);
   })
-  return answer;
 }
