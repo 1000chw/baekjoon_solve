c2c497b7e5a4245d19c6f7758d2728001cdf1a41
Chun Hyunwoo <1000chw@gmail.com>
2024-10-15 01:50:19 +0900

    [Platinum V] Title: 이진 트리, Time: 336 ms, Memory: 46540 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/31966. 이진 트리/이진 트리.py
+++ 백준/Platinum/31966. 이진 트리/이진 트리.py
@@ -1,6 +1,6 @@
 import sys
 input = sys.stdin.readline
-
+MOD = 1000000007
 n = int(input())
 nodes = [1] * (n+1)
 count = [1] * (n+1)
@@ -9,8 +9,8 @@ r_count = [1] * (n+1)
 
 for i in range(1, n+1):
     left, right = map(int, input().split())
-    l_count[i] = l_count[left] + l_count[right] + nodes[right] - 1
-    r_count[i] = r_count[right] + r_count[left] + nodes[left] - 1
-    nodes[i] = nodes[left] + nodes[right]
-    count[i] = count[left] + count[right] + r_count[left]*nodes[right] + l_count[right]*nodes[left] - 1
+    l_count[i] = (l_count[left] + l_count[right] + nodes[right] - 1) % MOD
+    r_count[i] = (r_count[right] + r_count[left] + nodes[left] - 1) % MOD
+    nodes[i] = (nodes[left] + nodes[right]) % MOD
+    count[i] = (count[left] + count[right] + r_count[left]*nodes[right] + l_count[right]*nodes[left] - 1) % MOD
     print(count[i])
